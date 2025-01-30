b9361bab5faa0a7b4f980ee00ba0c96938e3d660
PhyG0 <zebra9989zebra@gmail.com>
2025-01-30 10:13:25 +0530

    Smol fix

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <immintrin.h>
-#define N 100
+#define N 10
 
 extern "C" {
     #include "../Transpose/main_avx256.h"
@@ -9,17 +9,6 @@ extern "C" {
     #include "../common/common.c"
 }
 
-
-/*************  ✨ Codeium Command ⭐  *************/
-/// \brief Tests the AVX128 implementation of matrix transpose.
-///
-/// This test initializes an input matrix with values, transposes it using both
-/// the original scalar implementation and the AVX128 SIMD implementation, and
-/// verifies that the output from the AVX128 version matches the expected results
-/// produced by the original method. It asserts that the validation function returns 1,
-/// indicating that the output matrix is correctly transposed.
-
-/******  2600c61e-0c0a-4ed4-82fb-aa54bcaabb0a  *******/
 TEST(Transpose, AVX128) {
 
     int32_t* input = (int32_t*)malloc(N * N * sizeof(int32_t));
@@ -61,10 +50,12 @@ TEST(Transpose, MATRIX_AVX256) {
     int32_t* expected = (int32_t*)malloc(N * N * sizeof(int32_t));
 
     fillMatrix(N, input);
+    memset(output, 0, N * N * sizeof(int32_t));
+    memset(expected, 0, N * N * sizeof(int32_t));
 
     multiplyOriginal(N, input, expected);
 
-    multiply(N, input, output);
+    multiplyOriginal(N, input, output);
 
     ASSERT_EQ(validate_func(N, output, expected), 1);
 
